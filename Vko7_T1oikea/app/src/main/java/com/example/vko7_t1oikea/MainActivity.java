35c8d5912d957e192c5d0732435d0e04fddaf3cf
juhov <juho-ville.koivistoinen@student.lut.fi>
2020-02-29 23:07:30 +0200

    lisätään projektikansiot

---
+++ Vko7_T1oikea/app/src/main/java/com/example/vko7_t1oikea/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.vko7_t1oikea;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void button(View v) {
+        System.out.println("Hello World!");
+    }
+}
+
