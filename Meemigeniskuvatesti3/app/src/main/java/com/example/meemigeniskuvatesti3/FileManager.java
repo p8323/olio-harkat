f95bfe410587252b81b2f8d6b873623304bd5311
juhov <juho-ville.koivistoinen@student.lut.fi>
2020-04-04 18:28:52 +0300

    lisätään testiprojekti harkkatyöhön

--- Meemigeniskuvatesti3/app/src/main/java/com/example/meemigeniskuvatesti3/FileManager.java
+++ Meemigeniskuvatesti3/app/src/main/java/com/example/meemigeniskuvatesti3/FileManager.java
@@ -1,28 +1,24 @@
-package com.example.meemigeniskuvatesti2;
+package com.example.meemigeniskuvatesti3;
 
 import android.Manifest;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
-import android.net.Uri;
-
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
-
-public class FileManager {
+public class FileManager extends Activity {
 
     private int GALLERY_REQUEST_CODE = 1;
-    private Uri selectedImage = null;
     private static FileManager instance = null;
 
-    public Uri getImage(Context context) { //this method uses context from MainActivity
-        if (ContextCompat.checkSelfPermission((Activity)context, Manifest.permission.READ_EXTERNAL_STORAGE) !=
+    public void loadImage(Context context) { //uses context of MainActivity
+        if (ContextCompat.checkSelfPermission(context, Manifest.permission.READ_EXTERNAL_STORAGE) !=
                 PackageManager.PERMISSION_GRANTED) {
             requestGalleryPermission(context);
         }
-        if (ContextCompat.checkSelfPermission((Activity)context, Manifest.permission.READ_EXTERNAL_STORAGE) ==
+        if (ContextCompat.checkSelfPermission(context, Manifest.permission.READ_EXTERNAL_STORAGE) ==
                 PackageManager.PERMISSION_GRANTED) {
             Intent photopickerintent = new Intent(Intent.ACTION_PICK);
             photopickerintent.setType("image/*");
@@ -30,20 +26,11 @@ public class FileManager {
             //photopickerintent.putExtra(Intent.EXTRA_MIME_TYPES, mimeTypes);
             ((Activity)context).startActivityForResult(photopickerintent, GALLERY_REQUEST_CODE);
         }
-        return selectedImage;
-    }
-
-    protected void onActivityResult(int requestcode, int resultcode, Intent data) {
-        if (resultcode == Activity.RESULT_OK) {
-            if (requestcode == GALLERY_REQUEST_CODE) {
-                selectedImage = data.getData();
-            }
-        }
     }
 
     private void requestGalleryPermission(Context context) { //TODO lisää shouldShowRequestPermissionRationale jos tulee ongelmia
         String[] permissions = {Manifest.permission.READ_EXTERNAL_STORAGE};
-        ActivityCompat.requestPermissions((Activity) context, permissions, GALLERY_REQUEST_CODE);
+        ActivityCompat.requestPermissions((Activity)context, permissions, GALLERY_REQUEST_CODE);
     }
 
     public static FileManager getInstance() {
--- Meemigeniskuvatesti3/app/src/main/java/com/example/meemigeniskuvatesti3/MainActivity.java
+++ Meemigeniskuvatesti3/app/src/main/java/com/example/meemigeniskuvatesti3/MainActivity.java
@@ -1,20 +1,22 @@
-package com.example.meemigeniskuvatesti2;
+package com.example.meemigeniskuvatesti3;
 
 import androidx.appcompat.app.AppCompatActivity;
+import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 
-
-
 public class MainActivity extends AppCompatActivity {
 
     private ImageView meme;
     private Button button;
     FileManager filemanager = null;
+    private int GALLERY_REQUEST_CODE = 1;
+    private Uri selectedImage;
     Context maincontext = this;
 
     @Override
@@ -27,9 +29,16 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void createMeme(View v) {
-        Uri image = filemanager.getImage(maincontext);
-        meme.setImageURI(image); //sets image on screen
+        filemanager.loadImage(maincontext); //loadImage activates onActivityResult
+    }
+
+    protected void onActivityResult(int requestcode, int resultcode, Intent data) {
+        if (resultcode == Activity.RESULT_OK) {
+            if (requestcode == GALLERY_REQUEST_CODE) {
+                selectedImage = data.getData();
+                meme.setImageURI(selectedImage); //sets the image on screen
+            }
+        }
     }
 
 }
-
